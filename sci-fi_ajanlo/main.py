2bc26fea058403c5eb91a30caddc1596295ae1a1
supernauth <supynauth@gmail.com>
2023-12-04 13:38:09 +0100

    Handling wrong input

--- sci-fi_ajanlo/main.py
+++ sci-fi_ajanlo/main.py
@@ -7,18 +7,31 @@ ajanlo_app.üdvözlés()
 for i in ajanlo_app.alműfaj_nyomtató():
     print(i)
 
+menő_műfaj = ["űropera", "hard sci-fi"]
+# kevésbé_menő_műfaj = ajanlo_app.alműfaj_nyomtató().remove("űropera")
+
 választott_alműfaj = input("\nMelyik műfajból szeretnél választani? A fentiek közül tudsz választani: ")
 
 # Lekezelni, hogy ha nem jó a bevitt szöveg
+jó_választás = True
 
-if választott_alműfaj == "űropera" or választott_alműfaj == "hard sci-fi":
-    print("\nRemek választás! A következő könyvet tudom ajánlani:")
-else:
-    print("\nRendben, a lenti könyvet ajánlom. Azpért vess egy pillantást az űropera vagy hard sci-fi felé is!")
+while jó_választás:
+    try:
+        if választott_alműfaj == "űropera" or választott_alműfaj == "hard sci-fi":
+            print("\nRemek választás! A következő könyvet tudom ajánlani:")
+        else:
+            print("\nRendben, a lenti könyvet ajánlom. Azpért vess egy pillantást az űropera vagy hard sci-fi felé is!")
 
-random_könyv = ajanlo_app.véletlen_szám_sorsoló(választott_alműfaj)
-olvasnivaló = ajanlo_app.műfaj_véletlen_könyve(választott_alműfaj, random_könyv)
-print(olvasnivaló)
-ajanlo_app.könyv_feljegyzése(választott_alműfaj, random_könyv)
-ajanlo_app.ajánlás_hely_kiíró()
-ajanlo_app.elköszönő()
+            random_könyv = ajanlo_app.véletlen_szám_sorsoló(választott_alműfaj)
+            olvasnivaló = ajanlo_app.műfaj_véletlen_könyve(választott_alműfaj, random_könyv)
+            print(olvasnivaló)
+            ajanlo_app.könyv_feljegyzése(választott_alműfaj, random_könyv)
+            ajanlo_app.ajánlás_hely_kiíró()
+            ajanlo_app.elköszönő()
+        break
+    except:
+        print("Hibás alműfaj.")
+        választott_alműfaj = input("Próbáld újra: ")
+
+
+
